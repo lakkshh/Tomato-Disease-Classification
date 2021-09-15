c344def65640aa6153341bd479cc36d9a2bcb2fe
Lakshya Gupta <43241138+lakkshh@users.noreply.github.com>
2021-09-15 22:42:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import tensorflow as tf
 from tensorflow.keras import models, layers
 import numpy as np
-import cv2
+from cv2 import *
 import shutil
 
 app = Flask(__name__)
