1716233c58db5410187b149b0d952e479b47f05d
Lakshya Gupta <43241138+lakkshh@users.noreply.github.com>
2021-09-15 22:32:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,3 +41,6 @@ def prediction():
 	predicted_class, confidence = predict(model, image)
 
 	return render_template("prediction.html", data=predicted_class, conf=confidence)
+
+if __name__ == "__main__":
+	app.run(debug=True)
